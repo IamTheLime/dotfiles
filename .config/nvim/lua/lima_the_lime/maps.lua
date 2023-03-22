b38b1554e5c1fa7237c6f42bb67ed58aee42ae8d
Tiago Lima <tiago@desktop.com>
2023-03-22 16:48:22 +0000

    better horizontal spliting

--- .config/nvim/lua/lima_the_lime/maps.lua
+++ .config/nvim/lua/lima_the_lime/maps.lua
@@ -18,7 +18,7 @@ keymap.set('n', '<C-a>', 'gg<S-v>G')
 -- New tab
 keymap.set('n', 'te', ':tabedit')
 -- Split window
-keymap.set('n', 'ss', ':split<Return><C-w>w')
+keymap.set('n', 'sh', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')

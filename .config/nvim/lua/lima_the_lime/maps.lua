d6071c6519d8bbbf0b3720fd1b27fcf1e18f4eec
Tiago Lime, The Lime <tiago@myurbanjungle.com>
2023-03-23 11:21:34 +0000

    No more keymap for move window]

--- .config/nvim/lua/lima_the_lime/maps.lua
+++ .config/nvim/lua/lima_the_lime/maps.lua
@@ -22,7 +22,7 @@ keymap.set('n', 'sh', ':split<Return><C-w>w')
 keymap.set('n', 'sv', ':vsplit<Return><C-w>w')
 -- Move window
 keymap.set('n', '<Space>', '<C-w>w')
-keymap.set('', 'sh', '<C-w>h')
+--keymap.set('', 'sh', '<C-w>h')
 keymap.set('', 'sk', '<C-w>k')
 keymap.set('', 'sj', '<C-w>j')
 keymap.set('', 'sl', '<C-w>l')

1eddd6a6d86e7cd3a5ea18746a5f66833abefe35
Tiago Lima <tiago@desktop.com>
2023-03-26 02:35:45 +0100

    lsp

--- .config/nvim/after/plugin/lsp-zero.rc.lua
+++ .config/nvim/after/plugin/lsp-zero.rc.lua
@@ -1,3 +1,36 @@
+-- DEFAULT KEYBINDS
+--
+--    K: Displays hover information about the symbol under the cursor
+--    in a floating window. See :help vim.lsp.buf.hover().
+--
+--    gd: Jumps to the definition of the symbol under the cursor.
+--    See :help vim.lsp.buf.definition().
+--
+--    gD: Jumps to the declaration of the symbol under the cursor.
+--    Some servers don't implement this feature. See :help vim.lsp.buf.declaration().
+--
+--    gi: Lists all the implementations for the symbol under the cursor in the quickfix window.
+--    See :help vim.lsp.buf.implementation().
+--
+--    go: Jumps to the definition of the type of the symbol under the cursor.
+--    See :help vim.lsp.buf.type_definition().
+--
+--    gr: Lists all the references to the symbol under the cursor in the quickfix window.
+--    See :help vim.lsp.buf.references().
+--
+--    <Ctrl-k>: Displays signature information about the symbol under the cursor in a floating window. See :help vim.lsp.buf.signature_help(). If a mapping already exists for this key this function is not bound.
+--
+--    <F2>: Renames all references to the symbol under the cursor.
+--    See :help vim.lsp.buf.rename().
+--
+--
+--    <F4>: Selects a code action available at the current cursor position. See :help vim.lsp.buf.code_action().
+--
+--    gl: Show diagnostics in a floating window. See :help vim.diagnostic.open_float().
+--
+--    [d: Move to the previous diagnostic in the current buffer. See :help vim.diagnostic.goto_prev().
+--
+--    ]d: Move to the next diagnostic. See :help vim.diagnostic.goto_next().
 
 local lsp = require('lsp-zero').preset({
   name = 'recommended',
@@ -7,6 +40,12 @@ local lsp = require('lsp-zero').preset({
 })
 
 lsp.nvim_workspace()
+
+lsp.on_attach(function(client, bufnr)
+  vim.keymap.set({ 'n', 'x' }, 'gq', function()
+    vim.lsp.buf.format({ async = false, timeout_ms = 10000 })
+  end)
+end)
 lsp.format_on_save({
   servers = {
     ['lua_ls'] = {'lua'},
@@ -14,24 +53,30 @@ lsp.format_on_save({
   }
 })
 
+-- This works, but forces every buffer to be formatted in full on save
+-- lsp.on_attach(function(client, bufnr)
+--  lsp.buffer_autoformat()
+-- end)
+
 lsp.setup()
 
 vim.diagnostic.config({
-    virtual_text = true,
-    signs = true,
-    update_in_insert = true,
-    underline = true,
-    severity_sort = false,
-    float = true,
+  virtual_text = true,
+  signs = true,
+  update_in_insert = true,
+  underline = false,
+  severity_sort = false,
+  float = true,
 })
 
 
 
 local null_ls = require("null-ls")
 local null_opts = lsp.build_options('null-ls', {})
--- Note: I might want toremove this in the future 
+-- Note: I might want toremove this in the future
 null_ls.setup({
   on_attach = function(client, bufnr)
+    print(vim.inspect(client), bufnr)
     local lsp_format_modifications = require("lsp-format-modifications")
     lsp_format_modifications.attach(client, bufnr, { format_on_save = true })
     null_opts.on_attach(client, bufnr)
@@ -52,4 +97,3 @@ require('mason-null-ls').setup({
 
 -- Required when `automatic_setup` is true
 require('mason-null-ls').setup_handlers()
-

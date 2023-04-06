2fc9109b952cce3a90d5f5612e9844702bca239f
Tiago Lime, The Lime <tiago@myurbanjungle.com>
2023-04-06 10:42:04 +0100

    fixed stupid syntax

--- .config/nvim/after/plugin/treesitter.rc.lua
+++ .config/nvim/after/plugin/treesitter.rc.lua
@@ -1,1 +1,9 @@
-
+require 'nvim-treesitter.configs'.setup {
+    ensure_installed = { "c", "lua", "vim", "typescript", "html", "markdown", "javascript", "rust", "vimdoc", "query" },
+    sync_install = false,
+    auto_install = true,
+    highlight = {
+        enable = true,
+        additional_vim_regex_highlighting = false,
+    },
+}
--- .config/nvim/lua/lima_the_lime/plugins.lua
+++ .config/nvim/lua/lima_the_lime/plugins.lua
@@ -1,79 +1,70 @@
 local status, packer = pcall(require, "packer")
 if (not status) then
-  print("Packer is not installed")
-  return
+    print("Packer is not installed")
+    return
 end
 
 vim.cmd [[packadd packer.nvim]]
 
 packer.startup(function(use)
-  use 'wbthomason/packer.nvim'
-  use 'nvim-lualine/lualine.nvim' -- Statusline
-  use 'nvim-lua/plenary.nvim'     -- Common utilities
-  use 'onsails/lspkind-nvim'      -- vscode-like pictograms
-  use {
-    'VonHeikemen/lsp-zero.nvim',
-    branch = 'v1.x',
-    requires = {
-      -- LSP Support
-      { 'neovim/nvim-lspconfig' },             -- Required
-      { 'williamboman/mason.nvim' },           -- Optional
-      { 'williamboman/mason-lspconfig.nvim' }, -- Optional
+    use 'wbthomason/packer.nvim'
+    use 'nvim-lualine/lualine.nvim' -- Statusline
+    use 'nvim-lua/plenary.nvim'     -- Common utilities
+    --use 'onsails/lspkind-nvim'    -- vscode-like pictograms
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        requires = {
+            -- LSP Support
+            { 'neovim/nvim-lspconfig' },             -- Required
+            { 'williamboman/mason.nvim' },           -- Optional
+            { 'williamboman/mason-lspconfig.nvim' }, -- Optional
 
-      -- Autocompletion
-      { 'hrsh7th/nvim-cmp' },         -- Required
-      { 'hrsh7th/cmp-nvim-lsp' },     -- Required
-      { 'hrsh7th/cmp-buffer' },       -- Optional
-      { 'hrsh7th/cmp-path' },         -- Optional
-      { 'saadparwaiz1/cmp_luasnip' }, -- Optional
-      { 'hrsh7th/cmp-nvim-lua' },     -- Optional
+            -- Autocompletion
+            { 'hrsh7th/nvim-cmp' },         -- Required
+            { 'hrsh7th/cmp-nvim-lsp' },     -- Required
+            { 'hrsh7th/cmp-buffer' },       -- Optional
+            { 'hrsh7th/cmp-path' },         -- Optional
+            { 'saadparwaiz1/cmp_luasnip' }, -- Optional
+            { 'hrsh7th/cmp-nvim-lua' },     -- Optional
 
-      -- Snippets
-      { 'L3MON4D3/LuaSnip' },             -- Required
-      { 'rafamadriz/friendly-snippets' }, -- Optional
+            -- Snippets
+            { 'L3MON4D3/LuaSnip' },             -- Required
+            { 'rafamadriz/friendly-snippets' }, -- Optional
+        }
     }
-  }
-  use "jose-elias-alvarez/null-ls.nvim"
-  use {
-    "jay-babu/mason-null-ls.nvim",
-  }
-  use 'joechrisellis/lsp-format-modifications.nvim'
-  use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
-  use 'williamboman/mason.nvim'
-  use 'williamboman/mason-lspconfig.nvim'
-  use { 'kartikp10/noctis.nvim', requires = { 'rktjmp/lush.nvim' } }
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
-  use({
-    "glepnir/lspsaga.nvim",
-    branch = "main",
-    requires = {
-      { "nvim-tree/nvim-web-devicons" },
-      { "nvim-treesitter/nvim-treesitter" }
+    use "jose-elias-alvarez/null-ls.nvim"
+    use {
+        "jay-babu/mason-null-ls.nvim",
     }
-  })
-  use 'L3MON4D3/LuaSnip'
-  use 'mbbill/undotree'
-  use {
-    'nvim-treesitter/nvim-treesitter',
-    run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
-  }
-  use 'kyazdani42/nvim-web-devicons' -- File icons
-  use 'nvim-telescope/telescope.nvim'
-  use 'nvim-telescope/telescope-file-browser.nvim'
-  use 'windwp/nvim-autopairs'
-  use 'windwp/nvim-ts-autotag'
-  use 'norcalli/nvim-colorizer.lua'
-  use 'folke/zen-mode.nvim'
-  use({
-    "iamcco/markdown-preview.nvim",
-    run = function() vim.fn["mkdp#util#install"]() end,
-  })
-  use 'akinsho/nvim-bufferline.lua'
-  -- use 'github/copilot.vim'
-  use { 'akinsho/git-conflict.nvim', tag = "*", config = function()
-    require('git-conflict').setup()
-  end }
-  use 'lewis6991/gitsigns.nvim'
-  use 'dinhhuy258/git.nvim' -- For git blame & browse
-  use "lukas-reineke/indent-blankline.nvim"
+    use 'joechrisellis/lsp-format-modifications.nvim'
+    use 'MunifTanjim/prettier.nvim' -- Prettier plugin for Neovim's built-in LSP client
+    use 'williamboman/mason.nvim'
+    use 'williamboman/mason-lspconfig.nvim'
+    use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use 'L3MON4D3/LuaSnip'
+    use 'mbbill/undotree'
+    use {
+        'nvim-treesitter/nvim-treesitter',
+        run = function() require('nvim-treesitter.install').update({ with_sync = true }) end,
+    }
+    use 'kyazdani42/nvim-web-devicons' -- File icons
+    use 'nvim-telescope/telescope.nvim'
+    use 'nvim-telescope/telescope-file-browser.nvim'
+    use 'windwp/nvim-autopairs'
+    use 'windwp/nvim-ts-autotag'
+    use 'norcalli/nvim-colorizer.lua'
+    use 'folke/zen-mode.nvim'
+    use({
+        "iamcco/markdown-preview.nvim",
+        run = function() vim.fn["mkdp#util#install"]() end,
+    })
+    use 'akinsho/nvim-bufferline.lua'
+    -- use 'github/copilot.vim'
+    use { 'akinsho/git-conflict.nvim', tag = "*", config = function()
+        require('git-conflict').setup()
+    end }
+    use 'lewis6991/gitsigns.nvim'
+    use 'dinhhuy258/git.nvim' -- For git blame & browse
+    use "lukas-reineke/indent-blankline.nvim"
 end)

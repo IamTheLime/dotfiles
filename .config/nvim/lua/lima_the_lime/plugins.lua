c504c2d3569be64b61d07fb4a701a24a57b84bc5
Tiago Lime, The Lime <tiago@myurbanjungle.com>
2023-04-15 11:36:11 +0100

    neogit

--- .config/nvim/lua/lima_the_lime/plugins.lua
+++ .config/nvim/lua/lima_the_lime/plugins.lua
@@ -67,4 +67,5 @@ packer.startup(function(use)
     use 'lewis6991/gitsigns.nvim'
     use 'dinhhuy258/git.nvim' -- For git blame & browse
     use "lukas-reineke/indent-blankline.nvim"
+    use { 'TimUntersberger/neogit', requires = 'nvim-lua/plenary.nvim' }
 end)

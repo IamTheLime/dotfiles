3c2bef01406d7309b3b2b46edc24ede7eaf4135b
Tiago Lima <tiago@desktop.com>
2023-04-30 20:42:19 +0100

    major changes to the dotfiles

--- .config/create_symbolic_links.sh
+++
@@ -1,4 +0,0 @@
-echo "Creating symbolic links for nvim and tmux";
-
-ln -s "$(pwd)/nvim" ~/.config
-ln -s "$(pwd)/tmux" ~/.config
---
+++ setup_environment.sh
@@ -0,0 +1,22 @@
+echo "First off lets install oh my zsh, that shit always creates a new zshrc which is annoying"
+# If oh-my-zsh is not install let's install it
+# Nothing safer than exec-ing a file straight from the web
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone https://github.com/Aloxaf/fzf-tab ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/fzf-tab
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+echo "Creatin symboluc link for zshrc"
+ln -s "$(pwd)/dotfiles/zshrc" ~/.zshrc
+
+echo "Creating symbolic links for nvim, tmux";
+
+ln -s "$(pwd)/dotfiles/nvim" ~/.config
+ln -s "$(pwd)/dotfiles/tmux" ~/.config
+
+
+echo "Cloning tpm"
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+
+# type this in terminal if tmux is already running
+tmux source ~/.config/tmux/tmux.conf

ea4153a40bc968513fadaa7d7bf4a6ab64c372ee
Tiago Lima <tiago@desktop.com>
2023-05-01 11:21:17 +0100

    installed stuff

--- setup_environment.sh
+++ setup_environment.sh
@@ -1,3 +1,20 @@
+read -q "REPLY?\ 
+Do you have the following installed ?
+
+- FD 
+- BAT
+- EXA
+- NVIM
+- TMUX
+
+IF NOT INSTALL THEM BEFORE YOU START THE PROCESS - they are in the README.md
+
+YOU WILL ALSO HAVE TO RUN THIS FILE TWICE, BECAUSE I CANNOT BE ARSED TO HANDLE OH_MY_ZSH
+"
+
+
+
+
 echo "First off lets install oh my zsh, that shit always creates a new zshrc which is annoying"
 # If oh-my-zsh is not install let's install it
 # Nothing safer than exec-ing a file straight from the web
@@ -7,12 +24,16 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 
 echo "Creatin symboluc link for zshrc"
-ln -sf "$(pwd)/dotfiles/zshrc" ~/.zshrc
+ln -sf "$(pwd)/dotfiles/zshrc" $HOME/.zshrc
+
+echo "Creating nvim installer"
+git clone --depth 1 https://github.com/wbthomason/packer.nvim ~/.local/share/nvim/site/pack/packer/start/packer.nvim
+nvim --headless -c 'autocmd User PackerComplete quitall' -c 'PackerSync'
 
 echo "Creating symbolic links for nvim, tmux";
 
-ln -sf "$(pwd)/dotfiles/nvim" ~/.config/nvim
-ln -sf "$(pwd)/dotfiles/tmux" ~/.config/tmux
+ln -sf "$(pwd)/dotfiles/nvim" $HOME/.config/nvim
+ln -sf "$(pwd)/dotfiles/tmux" $HOME/.config/tmux
 
 
 echo "Cloning tpm"

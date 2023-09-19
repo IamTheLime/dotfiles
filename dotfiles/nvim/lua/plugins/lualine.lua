0592c77cb82be7a3ef12ca00259b4b31fef46ba3
Tiago Lima <tiago@desktop.com>
2023-09-19 10:08:53 +0100

    tokyonight

--- dotfiles/nvim/lua/plugins/lualine.lua
+++ dotfiles/nvim/lua/plugins/lualine.lua
@@ -1,6 +1,7 @@
 return {
     'nvim-lualine/lualine.nvim', -- Statusline
     opts = {
+        theme = 'tokyonight-night',
         options = {
             icons_enabled = true,
             section_separators = { left = '░', right = '░' },
--- dotfiles/nvim/lua/plugins/rosepine.lua
+++ dotfiles/nvim/lua/plugins/rosepine.lua
@@ -47,7 +47,7 @@ return {
         }
     },
     name = 'rose-pine',
-    config = function()
-        vim.cmd('colorscheme rose-pine')
-    end
+    -- config = function()
+    --     vim.cmd('colorscheme rose-pine')
+    -- end
 }

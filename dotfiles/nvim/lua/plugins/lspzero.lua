2f6d926b4bb5779c414e8ceb51f18924e70bda46
Tiago Lima <tiago@desktop.com>
2023-09-24 12:49:55 +0100

    nvim dev

--- dotfiles/nvim/lua/plugins/lspzero.lua
+++ dotfiles/nvim/lua/plugins/lspzero.lua
@@ -143,6 +143,18 @@ return {
             return
         end
 
+        lspconfig.lua_ls.setup({
+            settings = {
+                Lua = {
+                    diagnostics = {
+
+                        globals = { 'vim' }
+
+                    }
+                }
+            }
+        })
+
         lspconfig.pyright.setup({
             settings = {
                 python = {

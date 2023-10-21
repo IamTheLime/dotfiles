4dc34acbac3656e5e9eaf31b9b283fee0f5ff339
Tiago Lime, The Lime <tiago@myurbanjungle.com>
2023-10-21 19:46:38 +0100

    jump windows

---
+++ dotfiles/nvim/lua/plugins/nvimwindow.lua
@@ -0,0 +1,22 @@
+return {
+    'yorickpeterse/nvim-window',
+    opts = {
+        chars = {
+            'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+            'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'
+        },
+
+        -- A group to use for overwriting the Normal highlight group in the floating
+        -- window. This can be used to change the background color.
+        normal_hl = 'Normal',
+
+        -- The highlight group to apply to the line that contains the hint characters.
+        -- This is used to make them stand out more.
+        hint_hl = 'Bold',
+
+        -- The border style to use for the floating window.
+        border = 'single'
+    },
+    keys = {
+        { "<Leader>jw", mode = { "n", "v" }, function() require("nvim-window").pick() end, desc = "Flash" } },
+}

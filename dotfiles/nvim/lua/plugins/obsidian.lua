0e59b0ffa5e7b212cea36163e41dec1273af7e0c
Tiago Lime, The Lime <tiago@myurbanjungle.com>
2023-10-24 13:23:31 +0100

    fixed obsidian

--- dotfiles/nvim/lua/plugins/obsidian.lua
+++ dotfiles/nvim/lua/plugins/obsidian.lua
@@ -1,11 +1,21 @@
+local function get_obsidian_vault_location()
+    if vim.loop.os_uname().sysname == "Darwin" then
+        return
+        "/Users/tiagolima/Library/CloudStorage/GoogleDrive-tafl.tiagolima@gmail.com/Other computers/My computer/notes/tiago_vault"
+    end
+    return ""
+end
+
+
+
 return {
     "epwalsh/obsidian.nvim",
     lazy = true,
     event = {
         -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
         -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
-        "BufReadPre /Users/tiagolima/Library/CloudStorage/GoogleDrive-tafl.tiagolima@gmail.com/Other computers/My computer/notes/tiago's vault/**.md",
-        "BufNewFile /Users/tiagolima/Library/CloudStorage/GoogleDrive-tafl.tiagolima@gmail.com/Other computers/My computer/notes/tiago's vault/**.md",
+        "BufReadPre " .. get_obsidian_vault_location() .. "/**.md",
+        "BufNewFile " .. get_obsidian_vault_location() .. "/**.md",
     },
     dependencies = {
         -- Required.
@@ -15,8 +25,7 @@ return {
     },
     opts = {
 
-        dir =
-        "/Users/tiagolima/Library/CloudStorage/GoogleDrive-tafl.tiagolima@gmail.com/Other computers/My computer/notes/tiago's vault"
+        dir = get_obsidian_vault_location()
         -- see below for full list of options 👇
     },
 }

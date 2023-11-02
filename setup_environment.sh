33930d73755e721ab0878d7f040b517f0229c871
Tiago Lime, The Lime <tiago@myurbanjungle.com>
2023-11-02 23:28:21 +0100

    zellij

--- setup_environment.sh
+++ setup_environment.sh
@@ -28,15 +28,5 @@ echo "Creating symbolic links for nvim, tmux";
 mkdir -p ~/.config
 
 ln -sf "$(pwd)/dotfiles/nvim" ~/.config
-ln -sf "$(pwd)/dotfiles/tmux" ~/.config
+ln -sf "$(pwd)/dotfiles/zellij" ~/.config
 
-
-echo "Cloning tpm"
-rm -rf ~/.config/tmux/plugins/
-
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/
-
-tmux start-server
-tmux new-session -d -A -s main;
-~/.config/tmux/plugins/bin/install_plugins 
-tmux kill-server

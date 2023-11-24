b400581389f22091a927ec1a13bb366a0b335686
Tiago Lime, The Lime <tiago@myurbanjungle.com>
2023-11-24 09:35:21 +0000

    dotfiles

--- docker_build_local.sh
+++ docker_build_local.sh
@@ -1,1 +1,5 @@
-docker build -t localconfig .
+# docker build -t localconfig .
+
+IMAGE_NAME=$(uuidgen | awk '{print tolower($0)}')
+docker build -t ttl.sh/${IMAGE_NAME}:24h .
+docker push ttl.sh/${IMAGE_NAME}:24h

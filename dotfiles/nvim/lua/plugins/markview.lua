0d3125c1815627a477c242c2244de151d89b705a
LimaTheLime <tafl.tiagolima@gmail.com>
2024-10-04 21:24:38 +0100

    markview

--- dotfiles/nvim/lua/plugins/markview.lua
+++ dotfiles/nvim/lua/plugins/markview.lua
@@ -1,6 +1,6 @@
 return {
     "OXY2DEV/markview.nvim",
-    lazy = true, -- Recommended
+    lazy = false, -- Recommended
     -- ft = "markdown" -- If you decide to lazy-load anyway
     dependencies = {
         -- You will not need this if you installed the

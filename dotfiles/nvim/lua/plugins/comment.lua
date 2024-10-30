1df053aafdcb6447ab5a308f8043f05a931c789c
Tiago Lime, The Lime <tiago@myurbanjungle.com>
2024-10-30 08:59:43 +0000

    fixed comments for angular files

--- dotfiles/nvim/lua/plugins/comment.lua
+++ dotfiles/nvim/lua/plugins/comment.lua
@@ -1,11 +1,18 @@
 return {
     'numToStr/Comment.nvim',
+    lazy = false,
     opts = {
         languages = {
             python = {
                 template = {
-                    annotation_convention = 'numpydoc' }
+                    annotation_convention = 'numpydoc'
+                }
             }
         }
     },
+    config = function()
+        local ft = require("Comment.ft")
+        ft.set("htmlangular", ft.get("html"))
+        require("Comment").setup()
+    end
 }
